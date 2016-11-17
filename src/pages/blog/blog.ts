1d89498f6e5ba0a165d55f19828e660d3ccfb8f6
Fengda HUANG <h@phodal.com>
2016-11-17 08:49:24 +0800

    add blog lists

--- src/pages/blog/blog.ts
+++ src/pages/blog/blog.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-blog',
   templateUrl: 'blog.html'
 })
 export class BlogPage {
+  private blogs;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,public http:Http) {
+    let url = 'http://localhost:8000/api/blog/';
+    this.http.get(url)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.blogs = data['results'];
+      });
   }
 
 }
