486d60681f7297c109a8a0a4bad4d1e807fb1992
Fengda HUANG <h@phodal.com>
2016-12-06 10:44:26 +0800

    add a login action

--- src/pages/center/center.ts
+++ src/pages/center/center.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {NavController, Events} from 'ionic-angular';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'page-center',
   templateUrl: 'center.html'
 })
 export class CenterPage {
+  private user = {
+    name: '',
+    password: ''
+  };
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private events: Events, private http: Http) {
+    this.events = events;
+    this.http = http;
   }
 
+  loginForm() {
+    var self = this;
+    let data = {
+      username: this.user.name,
+      password: this.user.password
+    };
+
+    this.http.post("http://localhost:8000/api-token-auth/", data)
+      .map(response => response.json())
+      .subscribe(
+        data => {
+          self.events.publish("user:login", data);
+        },
+        error => {
+          self.events.publish("user:login:error");
+        }
+      );
+  }
 }
