a26878dbe01d0cdfc19664cacc14e9e79bf545a3
Fengda HUANG <h@phodal.com>
2016-12-06 10:08:46 +0800

    add infinite scroll

--- src/pages/blog/blog.ts
+++ src/pages/blog/blog.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
 import {BlogDetailPage} from "../blog-detail/blog-detail";
@@ -10,17 +10,41 @@ import {BlogDetailPage} from "../blog-detail/blog-detail";
 })
 export class BlogPage {
   private blogs;
+  private hasNext = false;
+  private nextUrl = '';
 
-  constructor(public navCtrl: NavController,public http:Http) {
+  constructor(public navCtrl: NavController, public http: Http) {
     let url = 'http://localhost:8000/api/blog/';
     this.http.get(url)
       .map(res => res.json())
       .subscribe(data => {
+        this.hasNext = false;
+        if (data['next'] !== null) {
+          this.hasNext = true;
+          this.nextUrl = data['next'];
+        }
         this.blogs = data['results'];
       });
   }
 
-  gotoBlogDetail(blogId){
+  doInfinite(infiniteScroll) {
+    this.http.get(this.nextUrl)
+      .map(res => res.json())
+      .subscribe(data => {
+        this.hasNext = false;
+        if (data['next'] !== null) {
+          this.hasNext = true;
+          this.nextUrl = data['next'];
+        }
+        console.log(this.hasNext);
+        for (var i = 0; i < data['results'].length; i++) {
+          this.blogs.push(data['results'][i]);
+        }
+        infiniteScroll.complete();
+      });
+  }
+
+  gotoBlogDetail(blogId) {
     this.navCtrl.push(BlogDetailPage, {
       blogId: blogId
     });
