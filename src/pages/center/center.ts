eb6e3ab70c701013e4b64e484ec70e932a63d48f
Fengda HUANG <h@phodal.com>
2016-12-09 12:40:13 +0800

    rename data to userinfo

--- src/pages/center/center.ts
+++ src/pages/center/center.ts
@@ -24,12 +24,12 @@ export class CenterPage {
   }
 
   loginForm() {
-    let data = {
+    let userInfo = {
       username: this.user.name,
       password: this.user.password
     };
 
-    this.http.post("http://localhost:8000/api-token-auth/", data)
+    this.http.post("http://localhost:8000/api-token-auth/", userInfo)
       .map(response => response.json())
       .subscribe(
         data => {
